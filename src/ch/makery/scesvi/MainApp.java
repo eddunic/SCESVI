1300978da187a6453e6f6203768ff631edafd98b
eddunic <eduardo.bitencourt007@gmail.com>
2018-10-04 11:01:33 -0300

    Conexão com o SceneBuilder

--- src/ch/makery/scesvi/MainApp.java
+++ src/ch/makery/scesvi/MainApp.java
@@ -1,13 +1,57 @@
 package ch.makery.scesvi;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
+	
+	Stage primaryStage;
+	BorderPane rootLayout;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Login");
+		initRootLayout();
+		showLoginOverview();
+	}
+	
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public void showLoginOverview() {
+	    try {
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainApp.class.getResource("view/LoginScreen.fxml"));
+	        AnchorPane loginScreen = (AnchorPane) loader.load();
+
+	        // Define a overview no centro do root layout.
+	        rootLayout.setCenter(loginScreen);
+
+	        // Dá ao controlador acesso à the main app.
+	        //PersonOverviewController controller = loader.getController();
+	        //controller.setMainApp(this);
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 
 	public static void main(String[] args) {
