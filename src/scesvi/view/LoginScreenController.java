16b80f730789165e17635bc616bf151a752e56c2
eddunic <eduardo.bitencourt007@gmail.com>
2018-10-18 19:13:12 -0300

    Troca de cenas funcionando

--- src/scesvi/view/LoginScreenController.java
+++ src/scesvi/view/LoginScreenController.java
@@ -3,11 +3,19 @@ package scesvi.view;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import scesvi.MainApp;
 
 public class LoginScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private JFXTextField tName;
@@ -23,12 +31,17 @@ public class LoginScreenController {
 
     @FXML
     private Label lblRegister;
-
-	private MainApp mainApp;
     
-	public void setMainApp(MainApp mainApp) {
+    private MainApp mainApp;
+
+    public void btRegisterAction() {
+    	MainApp.changeScreen("UserRegister");
+    }
+    
+    public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
-
+    
 }
+
 
